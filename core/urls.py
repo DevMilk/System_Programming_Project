e6a5b310c8b3e965f096c29f5d9c816153d36071
DevMilk <ugurkkkkkk@hotmail.com>
2020-05-09 18:29:03 +0300

    Added navbar activities

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,7 @@ app_name = 'core'
 #Url adresleri burada belirtiliyor, bir sayfa eklemek istediğimizde burada onun url adresini belirtiyoruz
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('category=<str:slug>/', HomeView.as_view(), name='filtered'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
